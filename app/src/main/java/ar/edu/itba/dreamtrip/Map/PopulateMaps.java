68bd4aae1b49c50f4614cb36cd3a6b2cee56dcb0
jcaracciolo <jcaracciolo@itba.edu.ar>
2016-11-28 11:18:58 -0300

    Close to connect buttons on map

--- app/src/main/java/ar/edu/itba/dreamtrip/Map/PopulateMaps.java
+++ app/src/main/java/ar/edu/itba/dreamtrip/Map/PopulateMaps.java
@@ -16,6 +16,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.android.SphericalUtil;
 
@@ -23,6 +24,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Objects;
 
@@ -52,6 +54,24 @@ public class PopulateMaps extends AsyncTaskInformed<Object,Void,ArrayList<Collec
     public Context context;
     public GoogleMap map;
     public LatLng location;
+
+    public Collection<Marker> mAirports=new ArrayList<>();
+    public Collection<Marker> mCities=new ArrayList<>();
+    public Collection<Marker> mFLights=new ArrayList<>();
+    public Collection<Marker> mDeals=new ArrayList<>();
+
+    public Collection<MarkerOptions> mOpAirports=new ArrayList<>();
+    public Collection<MarkerOptions> mOpCities=new ArrayList<>();
+    public Collection<MarkerOptions> mOpFLights=new ArrayList<>();
+    public Collection<MarkerOptions> mOpDeals=new ArrayList<>();
+
+    public Collection<PolylineOptions> lOpFLights=new ArrayList<>();
+    public Collection<PolylineOptions> lOpDeals=new ArrayList<>();
+
+    public Collection<Polyline> lFLights=new ArrayList<>();;
+    public Collection<Polyline> lDeals=new ArrayList<>();
+
+    public HashMap<MarkerOptions,Trace> animated=new HashMap<>();
 
     public PopulateMaps(Context context, GoogleMap map, LatLng location) {
         this.context = context;
@@ -90,14 +110,14 @@ public class PopulateMaps extends AsyncTaskInformed<Object,Void,ArrayList<Collec
             MarkerOptions marker = new MarkerOptions().position(new LatLng(a.getLocation().getLatitude(),a.getLocation().getLongitude()));
             marker.title(a.getName());
             marker.icon(getMarkerIconFromDrawable(context.getDrawable(R.drawable.ic_place),1.5));
-            map.addMarker(marker);
+            mOpAirports.add(marker);
         }
 
         for (City c: cities) {
             MarkerOptions marker = new MarkerOptions().position(new LatLng(c.getLocation().getLatitude(),c.getLocation().getLongitude()));
             marker.title(c.getName());
             marker.icon(getMarkerIconFromDrawable(context.getDrawable(R.drawable.ic_city),0.5));
-            map.addMarker(marker);
+            mOpCities.add(marker);
         }
 
         DataHolder data= DataHolder.getInstance(context);
@@ -111,26 +131,56 @@ public class PopulateMaps extends AsyncTaskInformed<Object,Void,ArrayList<Collec
             PolylineOptions route = new PolylineOptions().add(origLat).add(destLat);
             route.color(TrackedFlightCardView.getColor(context,f.getStatus()));
             map.addPolyline(route);
+
             MarkerOptions marker = new MarkerOptions();
             double percentage=getPercentage(f.getStatus());
             marker.position(getLatLngAtPercentage(origLat,destLat,percentage));
             marker.position(origLat);
             marker.title(f.getIdentifier());
-            //TODO animate only active flights
             marker.anchor(0.5f,0.5f);
             marker.icon(getMarkerIconFromDrawable(context.getDrawable(R.drawable.ic_airplane_l),1.5));
-            Marker mark=map.addMarker(marker);
-            mark.showInfoWindow();
-            mark.setRotation((float) SphericalUtil.computeHeading(origLat, destLat));
-            LatLngBounds.Builder builder = new LatLngBounds.Builder();
-                            builder.include(origLat).include(destLat);
-                            LatLngBounds bounds = builder.build();
-            MarkerAnimation.animateMarkerToHC(mark, destLat, new LatLngInterpolator.LinearFixed());
+            mOpFLights.add(marker);
 
+            if(f.getStatus()==FlightStatus.ACTIVE){
+                animated.put(marker,new Trace(origLat,destLat));
+            }
         }
+    }
 
+    public void deleteMarkers(Collection<Marker> markers){
+        for (Marker m:markers) {
+            m.remove();
+        }
+    }
 
+    public void addMarkers(Collection<MarkerOptions> moptions,Collection<Marker> markers){
+        markers.clear();
+        for(MarkerOptions o: moptions){
+            Marker m=map.addMarker(o);
+            markers.add(m);
+            Trace t=animated.get(o);
+            if(t!=null){
+                m.showInfoWindow();
+                m.setRotation((float) SphericalUtil.computeHeading(t.orig, t.dest));
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                builder.include(t.orig).include(t.dest);
+                LatLngBounds bounds = builder.build();
+                MarkerAnimation.animateMarkerToHC(m, t.dest, new LatLngInterpolator.LinearFixed());
+            }
+        }
+    }
 
+    public void addLines(Collection<PolylineOptions> pOptions,Collection<Polyline> lines){
+        lines.clear();
+        for(PolylineOptions l: pOptions){
+            lines.add(map.addPolyline(l));
+        }
+    }
+
+    public void deleteLines(Collection<Polyline> lines){
+        for(Polyline p: lines){
+            p.remove();
+        }
     }
 
     public BitmapDescriptor getMarkerIconFromDrawable(Drawable drawable,double size) {
@@ -150,8 +200,8 @@ public class PopulateMaps extends AsyncTaskInformed<Object,Void,ArrayList<Collec
     }
 
     public LatLng getLatLngAtPercentage(LatLng start,LatLng finish,double percentage){
-        double newX=(start.latitude + finish.latitude)*percentage;
-        double newY=(start.longitude + finish.longitude)*percentage;
+        double newX=start.latitude * (1-percentage) + finish.latitude*percentage;
+        double newY=start.longitude * (1-percentage) + finish.longitude*percentage;
         return new LatLng(newX,newY);
 
     }
@@ -164,11 +214,21 @@ public class PopulateMaps extends AsyncTaskInformed<Object,Void,ArrayList<Collec
             case CANCELLED:
                 percentage=0.05;
             case ACTIVE:
-                percentage=0.5;
+                percentage=0;
             case LANDED:
                 percentage=0.95;
         }
         return percentage;
+    }
+
+    private class Trace{
+        public LatLng orig;
+        public LatLng dest;
+
+        public Trace(LatLng orig, LatLng dest) {
+            this.orig = orig;
+            this.dest = dest;
+        }
     }
 
 }
